ebf1ab57a0751a62912b6cc2fdd8be7943aea30b
Sylvain <estevez.sylvain@gmail.com>
2012-11-10 21:45:07 +0100

    next...

--- src/titleextractor/KnownWordsRemover.java
+++ src/titleextractor/KnownWordsRemover.java
@@ -18,7 +18,7 @@ public class KnownWordsRemover {
     //----------Text Files containing known words to remove
     private static ArrayList<String> commons1;
                                                     //-----> words that have been recognized as 'ending words' (ex : 'donnie brasco dvdrip fr-dlmania'
-                                                    //-----> everything after 'dvdrip' can be removed)                                                    
+                                                    //-----> everything after and including 'dvdrip' can be removed)                                                    
     private static ArrayList<String> commons2;
                                                     //-----> 'lonely' words (means that these words will only be removed 
                                                     //-----> if they are placed between spaces or any other separator)                                                    
@@ -40,12 +40,19 @@ public class KnownWordsRemover {
     // This class respects singleton pattern, 'self' is its unique instance
     private static KnownWordsRemover self = null;
     
+    private static char[] separators;
+    
     //=======================================================================================//
     //                                                                       CONSTRUCTORS                                                                             //
     //=======================================================================================//
     
     private KnownWordsRemover() {
         loadLibraries();
+        
+        separators = new char[3];
+        separators[0] = ' ';
+        separators[0] = '-';
+        separators[0] = '/';
     }
     
     public static KnownWordsRemover getInstance() {
@@ -63,6 +70,8 @@ public class KnownWordsRemover {
     //--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
     //---------- The folowing three methods can be called to remove some particular words as explained upper
     
+    //--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    //----------
     public static ArrayList<String> removeCommons1(ArrayList<String> list) {
         ArrayList<String> results = new ArrayList<>();
         String temp, comm;
@@ -82,8 +91,40 @@ public class KnownWordsRemover {
         
     }
     
+    //--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    //----------
     public static ArrayList<String> removeCommons2(ArrayList<String> list) {
         ArrayList<String> results = new ArrayList<>();
+        String temp, comm, sub1, sub2;
+        char[] chars;
+        int index;
+        
+        for(int i = 0; i < list.size(); i++) {
+            temp = list.get(i);
+            chars = temp.toCharArray();
+            for(int j = 0; j < commons2.size(); j++) {
+                comm = commons2.get(j);
+                index = temp.indexOf(comm);
+                if(index >= 0) { //If word has been found
+                    if(index > 0 && (index + comm.length()) < temp.length()) { //Word is inside the string
+                        if(isSeparator(temp.charAt(index - 1)) && isSeparator(temp.charAt(index + comm.length()))) { //If word is surround by separators
+                            sub1 = temp.substring(0, index);
+                            sub2 = temp.substring(index + comm.length());
+                            temp = sub1 + sub2;                        
+                        }
+                    } else if(index == 0) { //Word is starting string
+                        if(isSeparator(temp.charAt(comm.length() + 1))) {
+                            temp = temp.substring(comm.length());
+                        }
+                    } else if((index + comm.length()) == temp.length()) { //Word is ending string
+                        if(isSeparator(temp.charAt(index - 1))) {
+                            temp = temp.substring(0, temp.length() - comm.length());
+                        }
+                    }
+                }                
+            }
+            results.add(temp);
+        }
         
         
         return results;
@@ -118,7 +159,6 @@ public class KnownWordsRemover {
         BufferedReader br;
         
         for(int i = 0; i < 3; i++) {
-            System.out.println("NEW FILE");
             String line;
             ArrayList<String> list = new ArrayList<>();
 
@@ -130,7 +170,6 @@ public class KnownWordsRemover {
 
                 //Read file line by line and add string to list
                 while((line = br.readLine()) != null) {
-                    System.out.println("-----> " + line);
                     list.add(line);
                 }
             } catch (IOException ex) {
@@ -144,6 +183,17 @@ public class KnownWordsRemover {
         commons1 = commons.get(0);
         commons2 = commons.get(1);
         commons3 = commons.get(2);
+    }
+    
+    //--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    //----------Tests if requested char is contained in separators list
+    private static boolean isSeparator(char s) {
+        for(int i = 0; i < separators.length; i++) {
+            if(s == separators[i]) {
+                return true;
+            }
+        }
+        return false;
     }
     
 
